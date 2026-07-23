51972b9590e27d5acc9cda18fd55c037f08add7a
Noah Schroeder <noah.schroeder@gmail.com>
2026-07-22 18:43:46 -0700

    test: add unit test for KvStore

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 use std::collections::HashMap;
 use std::sync::{Arc, RwLock};
 
+#[derive(Clone)]
 pub struct KvStore {
     db: Arc<RwLock<HashMap<String, Vec<u8>>>>,
 }
@@ -22,6 +23,42 @@ impl KvStore {
     pub fn get(&self, key: &str) -> Option<Vec<u8>> {
         let lock = self.db.read().unwrap();
         let value = lock.get(key);
-        return value.cloned()
+        value.cloned()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::thread;
+
+    #[test]
+    fn test_concurrent_inserts() {
+        let store = KvStore::new();
+        let mut handles = vec![];
+
+        for i in 0..100 {
+            let store_clone = store.clone(); 
+            let handle = thread::spawn(move || {
+                let key = format!("user_{}", i);
+                let value = vec![i as u8];
+
+                store_clone.set(key, value);
+            });
+            
+            handles.push(handle);
+        }
+
+        for handle in handles {
+            handle.join().unwrap();
+        }
+
+        for i in 0..100 {
+            let key = format!("user_{}", i);
+            let expected_value = vec![i as u8];
+            
+            let actual_value = store.get(&key).expect("Key should exist");
+            assert_eq!(actual_value, expected_value, "Data mismatch for key: {}", key);
+        }
     }
 }
