843f768caf89bfddbcd80b4a46864a3f64255b43
Noah Schroeder <noah.schroeder@gmail.com>
2026-07-28 18:02:25 -0700

    feat: write server skeleton

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -1,8 +1,30 @@
-use std::{net::TcpListener, print};
+use std::{net::TcpListener, print, println};
+use std::io::Read;
 use kv_store::{KvStore, Command};
 
 fn main() {
     let store = KvStore::new();
     let listner = TcpListener::bind("127.0.0.1:7878").unwrap();
     println!("KV Server listening on 127.0.0.1:7878...");
+
+    for stream_result in listner.incoming() {
+        let mut stream = stream_result.unwrap();
+        println!("Client connected.");
+
+        let mut buffer = Vec::new();
+        stream.read_to_end(&mut buffer).unwrap();
+        let cmd: Command = bincode::deserialize(&buffer).unwrap();
+
+        match cmd {
+            Command::Set { key: k, value: v } => {
+            println!("The client wants to store {} bytes under the key '{}'", v.len(), k);
+            store.set(k, v);
+        }
+    
+            Command::Get { key: k } => {
+            println!("The client is asking for the key '{}'", k);
+            store.get(&k);
+            }
+        }
+    }
 }
