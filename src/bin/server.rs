4a283b119627c7c010e38766682e9f7773814003
Noah Schroeder <noah.schroeder@gmail.com>
2026-08-13 12:11:48 -0700

    fix: bind listener to 0.0.0.0

--- src/bin/server.rs
+++ src/bin/server.rs
@@ -4,7 +4,7 @@ use tokio::io::{AsyncReadExt, AsyncWriteExt};
 
 #[tokio::main]
 async fn main() {
-    let listener = TcpListener::bind("127.0.0.1:7878").await.unwrap();
+    let listener = TcpListener::bind("0.0.0.0:7878").await.unwrap();
     println!("Async Server with WAL listening on port 7878");
 
     let store = KvStore::open().await;
