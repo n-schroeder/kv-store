0c047bc0ed909fc98399650e7ca5642e452bc4f3
Noah Schroeder <noah.schroeder@gmail.com>
2026-08-14 14:22:47 -0700

    test:  `get` command

--- src/bin/client.rs
+++ src/bin/client.rs
@@ -1,3 +1,5 @@
+use std::format;
+
 use kv_store::{Command, Response};
 use tokio::net::TcpStream;
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
@@ -40,6 +42,32 @@ async fn main() {
     for task in tasks {
         task.await.unwrap();
     }
+
+    let task_read = tokio::spawn(async move {
+        let mut stream = TcpStream::connect("192.168.1.120:7878").await.unwrap();
+
+        let cmd = Command::Get {
+            key: format!("key_{}", 99),
+        };
+
+        let payload = bincode::serialize(&cmd).unwrap();
+        let len_bytes = (payload.len() as u32).to_be_bytes();
+        
+        stream.write_all(&len_bytes).await.unwrap();
+        stream.write_all(&payload).await.unwrap();
+        
+        let mut resp_len_buf = [0u8; 4];
+        stream.read_exact(&mut resp_len_buf).await.unwrap();
+        
+        let resp_len = u32::from_be_bytes(resp_len_buf) as usize;
+        let mut resp_payload = vec![0u8; resp_len];
+        stream.read_exact(&mut resp_payload).await.unwrap();
+        
+        let response: Response = bincode::deserialize(&resp_payload).unwrap();
+        println!("Client 100 got: {:?}", response);
+    });
+
+    task_read.await.unwrap();
     
     println!("Load test complete");
 }
