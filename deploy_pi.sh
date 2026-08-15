f742ef2fc3adb1e3df560e161b569c680e7cc4eb
Noah Schroeder <noah.schroeder@gmail.com>
2026-08-14 18:44:04 -0700

    fix: revise pi deployment script

--- deploy_pi.sh
+++ deploy_pi.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
-PI="node0"
+set -e
+
+PI_TARGET="node0"
 
 echo "Cross-compiling for ARM64 (this will take a minute)..."
 sudo docker buildx build --platform linux/arm64 -t kv-server-arm64 -o type=docker,dest=kv-server-arm64.tar .
 
 echo "Transferring to Raspberry Pi..."
-scp kv-server-arm64.tar $PI:~/
+scp kv-server-arm64.tar $PI_TARGET:~/
 
 echo "Executing remote update on the Pi..."
-ssh $PI << 'EOF'
+ssh $PI_TARGET << 'EOF'
   sudo docker load -i kv-server-arm64.tar
-  sudo docker stop rpi-server
-  sudo docker rm rpi-server
+
+  sudo docker stop rpi-server || true
+  sudo docker rm rpi-server || true
+
   sudo docker run -d \
     -p 7878:7878 \
     -v ~/dev/data/kv-store:/app \
     --name rpi-server \
     kv-server-arm64
+ 
   echo "Update complete on Pi."
 EOF
 
